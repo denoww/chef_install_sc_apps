501ef4043eb51c0b766f913ba9bfdf2b21034ef9
Rodrigo <denoww@gmail.com>
2014-11-26 15:57:14 -0200

    testando bashaliases

--- recipes/default.rb
+++ recipes/default.rb
@@ -70,75 +70,75 @@ end
   # action :run
 # end
 
-bash "wcs-create-instance" do
+bash "run source bash" do
   user "vagrant"
   code <<-EOH        
     source #{bashrc}
   EOH
 end
 
-# Clone socket server
-socket_app_folder = "#{folder_apps}/socket_server"
-git socket_app_folder do
-  repository "git@github.com:denoww/socket-server-seucondominio.git"
-  revision "master"
-  action :sync
-  ssh_wrapper ssh_file_wrapper
-  user "vagrant"
-end
+# # Clone socket server
+# socket_app_folder = "#{folder_apps}/socket_server"
+# git socket_app_folder do
+#   repository "git@github.com:denoww/socket-server-seucondominio.git"
+#   revision "master"
+#   action :sync
+#   ssh_wrapper ssh_file_wrapper
+#   user "vagrant"
+# end
 
-# Clone seucondominio
-sc_app_folder = "#{folder_apps}/seucondominio"
-sc_app_repo = 'git@github.com:denoww/seucondominio.git'
-# sc_app_folder = "#{folder_apps}/rails"
-# sc_app_repo = 'https://github.com/railstutorial/sample_app.git'
-git sc_app_folder do
-  repository sc_app_repo
-  # revision "master"
-  revision "vagrant"
-  action :sync
-  ssh_wrapper ssh_file_wrapper
-  user "vagrant"
-end
+# # Clone seucondominio
+# sc_app_folder = "#{folder_apps}/seucondominio"
+# sc_app_repo = 'git@github.com:denoww/seucondominio.git'
+# # sc_app_folder = "#{folder_apps}/rails"
+# # sc_app_repo = 'https://github.com/railstutorial/sample_app.git'
+# git sc_app_folder do
+#   repository sc_app_repo
+#   # revision "master"
+#   revision "vagrant"
+#   action :sync
+#   ssh_wrapper ssh_file_wrapper
+#   user "vagrant"
+# end
 
 
-gem_package 'bundler' do
-  options '--no-ri --no-rdoc'
-end
+# gem_package 'bundler' do
+#   options '--no-ri --no-rdoc'
+# end
 
-rvm_shell "bundle" do
-  ruby_string node[:rvm][:default_ruby]
-  user        "root"
-  group       "root"
-  cwd         sc_app_folder
-  code        <<-EOF
-    bundle install
-  EOF
-end
+# rvm_shell "bundle" do
+#   ruby_string node[:rvm][:default_ruby]
+#   user        "root"
+#   group       "root"
+#   cwd         sc_app_folder
+#   code        <<-EOF
+#     bundle install
+#   EOF
+# end
 
-# config seucondominio
-tasks = ""
-case enviroment
-when "production"
-when "staging"
-when "development"
-  tasks << "echo 'Creating and feeding database';"
-  # development
-  tasks << "RAILS_ENV=development rake db:drop;"
-  tasks << "RAILS_ENV=development rake db:mongoid:drop;"
-  tasks << "RAILS_ENV=development rake db:setup;"
-  # test
-  tasks << "RAILS_ENV=test rake db:drop;"
-  tasks << "RAILS_ENV=test rake db:mongoid:drop;"
-  tasks << "RAILS_ENV=test rake db:setup;"
-end
+# # config seucondominio
+# tasks = ""
+# case enviroment
+# when "production"
+# when "staging"
+# when "development"
+#   tasks << "echo 'Creating and feeding database';"
+#   # development
+#   tasks << "RAILS_ENV=development rake db:drop;"
+#   tasks << "RAILS_ENV=development rake db:mongoid:drop;"
+#   tasks << "RAILS_ENV=development rake db:setup;"
+#   # test
+#   tasks << "RAILS_ENV=test rake db:drop;"
+#   tasks << "RAILS_ENV=test rake db:mongoid:drop;"
+#   tasks << "RAILS_ENV=test rake db:setup;"
+# end
   
-bash "sc_config" do
-  user "vagrant"
-  cwd  sc_app_folder
-  code <<-EOH
-    cp gitignore_sample .gitignore
-    cp config/application_sample.yml config/application.yml
-    #{tasks}
-  EOH
-end  
+# bash "sc_config" do
+#   user "vagrant"
+#   cwd  sc_app_folder
+#   code <<-EOH
+#     cp gitignore_sample .gitignore
+#     cp config/application_sample.yml config/application.yml
+#     #{tasks}
+#   EOH
+# end  
